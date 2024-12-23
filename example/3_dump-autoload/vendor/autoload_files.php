de380a43582307fabb4a537a5558867c375983c1
o0h <dev@o0h.in>
2024-12-23 12:04:54 +0900

    .gitignoreの整理

--- example/3_dump-autoload/vendor/autoload_files.php
+++
@@ -1,4 +0,0 @@
-<?php return array (
-  0 => '/opt/example/3_dump-autoload/vendor/symfony/polyfill-mbstring/bootstrap.php',
-  1 => '/opt/example/3_dump-autoload/vendor/symfony/polyfill-php84/bootstrap.php',
-);
